98679dc14c3ad4c11d3464144becaaaa9a082e77
COMLAN Giovanni <giovanni.comlan@yahoo.fr>
2022-08-16 14:50:25 +0200

    ft_itoa done not clean yet ???

--- Level/Level 4/ft_itoa/ft_itoa.c
+++ Level/Level 4/ft_itoa/ft_itoa.c
@@ -1,12 +1,60 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_split.c                                         :+:      :+:    :+:   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gcomlan <gcomlan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/11 18:06:11 by gcomlan           #+#    #+#             */
-/*   Updated: 2022/08/11 18:06:13 by gcomlan          ###   ########.fr       */
+/*   Updated: 2022/08/16 14:48:19 by gcomlan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
+#include <stdio.h>
+
+int	len_nb(long nbr)
+{
+	int	len;
+
+	len = 0;
+	if (nbr == 0)
+		return (1);
+	if (nbr < 0)
+		len++;
+	while (nbr != 0)
+	{
+		nbr = nbr / 10;
+		len++;
+	}
+	return (len);
+}
+
+char	*ft_itoa(int nbr)
+{
+	long	len;
+	long	tmp_len;
+	char	*res;
+
+	len = len_nb(nbr);
+	tmp_len = nbr;
+	if (nbr < 0)
+		tmp_len *= -1;
+	res = malloc(sizeof(char) * (len + 1));
+	if (!res)
+		return (NULL);
+	res[len] = 0;
+	if (tmp_len == 0)
+		res[0] = '0';
+	else
+	{
+		while (len--, tmp_len != 0)
+		{
+			res[len] = (tmp_len % 10) + '0';
+			tmp_len = (tmp_len - (tmp_len % 10)) / 10;
+		}
+		if (nbr < 0)
+			res[len] = '-';
+	}
+	return (res);
+}
--- Level/Level 4/ft_itoa/train.c
+++ Level/Level 4/ft_itoa/train.c
@@ -6,39 +6,39 @@
 /*   By: gcomlan <gcomlan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/11 18:06:16 by gcomlan           #+#    #+#             */
-/*   Updated: 2022/08/16 01:17:28 by gcomlan          ###   ########.fr       */
+/*   Updated: 2022/08/16 14:49:21 by gcomlan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <stdio.h>
 
-static int	len_nb(long nb)
+int	len_nb(long nbr)
 {
 	int	len;
 
 	len = 0;
-	if (nb == 0)
+	if (nbr == 0)
 		return (1);
-	if (nb < 0)
+	if (nbr < 0)
 		len++;
-	while (nb != 0)
+	while (nbr != 0)
 	{
-		nb = nb / 10;
+		nbr = nbr / 10;
 		len++;
 	}
 	return (len);
 }
 
-char	*ft_itoa(int n)
+char	*ft_itoa(int nbr)
 {
 	long	len;
 	long	tmp_len;
 	char	*res;
 
-	len = len_nb(n);
-	tmp_len = n;
-	if (n < 0)
+	len = len_nb(nbr);
+	tmp_len = nbr;
+	if (nbr < 0)
 		tmp_len *= -1;
 	res = malloc(sizeof(char) * (len + 1));
 	if (!res)
@@ -53,10 +53,23 @@ char	*ft_itoa(int n)
 			res[len] = (tmp_len % 10) + '0';
 			tmp_len = (tmp_len - (tmp_len % 10)) / 10;
 		}
-		if (n < 0)
+		if (nbr < 0)
 			res[len] = '-';
 	}
 	return (res);
+}
+
+int	main(void)
+{
+	printf("%s\n", ft_itoa(0));
+	printf("%s\n", ft_itoa(1));
+	printf("%s\n", ft_itoa(42));
+	printf("%s\n", ft_itoa(1001));
+	printf("%s\n", ft_itoa(-0));
+	printf("%s\n", ft_itoa(-2));
+	printf("%s\n", ft_itoa(-24));
+	printf("%s\n", ft_itoa(-2147483648));
+	printf("%s\n", ft_itoa(2147483647));
 }
 
 /*
